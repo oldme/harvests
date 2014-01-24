e9b82cacec047d260376be3b7510bdcb67ddc4fd
salboaie <salboaie@gmail.com>
2014-01-24 19:02:17 +0200

    let's do some tests ;)

--- test/penguins.js
+++ test/penguins.js
@@ -1,35 +1,89 @@
 //simulate the painting of a penguin children. paint the male babies blue and female babies pink if their mother asynchronously agree
 //test if all penguins got painted
 
-var harvest = require("harvest").newHarvest();
+var harvest = require("../lib/harvest.js").new();
+var assert  = require("assert");
 
-function loadPenguin(id,success, error){
-
+var penguins = {
+    "MrPenguin":{
+      nick:"MrPenguin"
+    },
+    "MrsPenguin":{
+        nick:"MrsPenguin"
+    },
+    "Alice":{
+        colour:"black",
+        sex:"female",
+        father:"MrPenguin",
+        mother:"MrsPenguin"
+    },
+    "Bob":{
+        colour:"green",
+        sex:"male",
+        father:"MrPenguin",
+        mother:"MrsPenguin"
+    },
+    "Charlie":{
+        colour:"black",
+        sex:"male",
+        father:"MrPenguin",
+        mother:"MrsPenguin"
+    },
+    "Dostoevsky":{
+        colour:"white",
+        sex:"male"
+    }
 }
 
-function loadPenguinFamily(father,mother, success, error){
-
+function loadPenguin(nick,success, error){
+    if(penguins[nick]){
+        success(penguins[nick]);
+    } else {
+        error(new Error("Penguin not available: " +nick));
+    }
 }
 
-function paintPenguin(id,newColour,success, error){
+function loadPenguinFamily(father, mother, success, error){
+        var ret = [];
+        for(var v in penguins){
+            if(penguins[v].father == father.nick && penguins[v].mother == mother.nick){
+                ret.push(penguins[v]);
+            }
+        }
+    success(ret);
+}
 
+function paintPenguin(penguin, newColour){
+    penguin.colour = newColour;
+}
+
+function childPenguinsShouldBeBlue(father, mother){
+    for(var v in penguins){
+        if(penguins[v].father == father.nick && penguins[v].mother == mother.nick){
+            if(penguins[v].sex == "male"){
+                assert.equal(penguins[v],"blue");
+            } else {
+                assert.equal(penguins[v],"pink");
+            }
+        }
+    }
 }
 
 function givePaintAgreement(parent,child,colour,success, error){
-
+    success(true);
 }
 
-harvest.load('@father',loadPenguin, 'MrPenguin');
-harvest.load('@mother',loadPenguin, 'MrsPenguin');
-harvest.load('@family',loadPenguinFamily, '@father', '@mother');
+harvest.load('father',loadPenguin, 'MrPenguin');
+harvest.load('mother',loadPenguin, 'MrsPenguin');
+harvest.load('family',loadPenguinFamily, '@father', '@mother');
 
 harvest.onSuccess(function(harvest){
     //harvest.family is an array with little penguins
         for(var at = 0; at < harvest.family.length; at++){
             var littlePenguin = harvest.family[at];
             var color = littlePenguin.sex == 'male'?'blue':'pink';
-            harvest.loadAt('@agreementFather', at, givePaintAgreement, '@father', littlePenguin, color);
-            harvest.loadAt('@agreementMother', at, givePaintAgreement, '@mother', littlePenguin, color);
+            harvest.loadAt('agreementFather', at, givePaintAgreement, wait('father'), littlePenguin, color);
+            harvest.loadAt('agreementMother', at, givePaintAgreement, wait('mother'), littlePenguin, color);
         }
 
     //now that we got or first phase, we can redefine the success to ever more
@@ -38,9 +92,12 @@ harvest.onSuccess(function(harvest){
                 var littlePenguin = harvest.family[i];
                 var color = littlePenguin.sex == 'male'?'blue':'pink';
                 if(harvest.agreementFather[i] && harvest.agreementMother[i]){
-                    harvest.do('paintPenguin',littlePenguin,color);
+                    harvest.do('paintPenguin',littlePenguin,color, wait(father));  // not used but could be useful
                 }
             }
+
+            harvest.set('paintingComplete', true);
+            harvest.do('childPenguinsShouldBeBlue',wait('father'), wait('mother'), wait('paintingComplete'));
         });
 });
 
@@ -48,3 +105,6 @@ harvest.onError = function(harvest){
     console.log("Well, move those penguins to the South Pole...");
 }
 
+setTimeout(function(){
+       assert.equal(harvest.paintingComplete, true);
+}, 100)
